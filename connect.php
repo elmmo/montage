b777396a47a014be86b2754e31e21210b395c758
ELIZABETH MIN <elmm682@gmail.com>
2019-07-28 00:10:10 -0700

    add exception handling

--- connect.php
+++ connect.php
@@ -1,16 +1,28 @@
 <?
+// load in packages 
 require_once 'vendor/autoload.php';
 
-echo getenv("DATABASE_URL"); 
+// initialize DotEnv for environment variables 
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
 
+// parse db connection info from env variable "
 $db = parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+// create connection 
+try {
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+    echo "Connected successfully.";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage() . "<br/>";
+    die(); 
+}
+
 ?>
