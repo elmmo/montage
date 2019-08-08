1bbd1946e95c643e8a24a97fab60c8e10bca1eb4
ELIZABETH MIN <elmm682@gmail.com>
2019-08-08 14:54:44 -0700

    temporary scheduler replacement

--- config/jwt.php
+++ config/jwt.php
@@ -71,7 +71,7 @@ class JWT {
             ->addSignature($this->key, ['alg' => 'HS256'])   // add a signature with a simple protected header
             ->build(); 
         // once key has been used, store in db
-        if ($this->db->storeKey($this->key, $id)) {
+        if ($this->db->removeExpiredKeys() && $this->db->storeKey($this->key, $id)) {
             return $this->serializer->serialize($jws, 0); // serialize the signature at index 0 (bc only have one signature)
         }
     }
